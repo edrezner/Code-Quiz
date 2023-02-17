d49a590b07e9db3222abbf783ea05c992f9be9c5
Seven <edrezner7@gmail.com>
2023-02-16 22:15:30 -0500

    Set up a listener event to start the timer, and also to hide the start button page and move to the first question. I am considering having the whole quiz take place in one div instead of three I had initially planned after talking with Diego (Instructor).

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,23 @@
 /* 1. clicking start quiz button starts 75 second timer
+*/
+var startPage = document.querySelector(".startPage")
+var firstQue = document.querySelector(".firstQue")
+var timeRemaining = 75;
+var start = document.querySelector(".start");
+var timer = document.querySelector(".timer");
+
+start.addEventListener("click", function starTime() {
+    var timerInterval = setInterval(function() {
+        timeRemaining--;
+        timer.textContent = "Time Remaining: " + timeRemaining;
+
+        if (timeRemaining === 0) {
+            clearInterval(timerInterval);
+            alert("Time is up!");
+        }
+    }, 1000);
+        startPage.style.display = "none";
+        firstQue.style.display = "block";
+});
+
+/* matches */
