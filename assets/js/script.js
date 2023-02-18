9873c146465c4bf69c7cee0d344ae27c04594d54
Seven <edrezner7@gmail.com>
2023-02-18 17:27:52 -0500

    After using AskBCS I was directed to make my quiz questions, choices, and answer into an object. I did so which made it so I had an easier time influencing the behavior of the elements within the function and more specifically the loops. Added an hr and p element that let the user know if they got an answer right or wrong.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,48 +1,79 @@
 /* 1. clicking start quiz button starts 75 second timer
 */
 var content = document.querySelector(".content")
+var mark = document.querySelector(".mark")
 var timeRemaining = 75;
 var start = document.querySelector(".start");
 var timer = document.querySelector(".timer");
-var newH2 = "";
 var newH2 = document.createElement("h2");
 var quizButton = "";
-var quizQue = ["Question1", "Question2", "Question3", "Question4", "Question5"];
-var quizAns = ["Correct1", "Wrong1", "Wrong2", "Wrong3", "Wrong4", "Correct2", "Wrong5", "Wrong6",
-    "Correct1", "Wrong7", "Wrong8", "Wrong9", "Wrong10", "Wrong11", "Wrong12", "Correct4", "Wrong13", "Wrong14", "Correct5", "Wrong15"];
+var newHr = document.createElement("hr");
+var newP = document.createElement("p");
+var quizBody = [
+    {
+        title: "Commonly used data types DO NOT include:",
+        choices: ["strings", "booleans", "alerts", "numbers",],
+        answer: 'alerts',
+    },
+    {
+        title: "The condition in an if / else statement is enclosed within ____.",
+        choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
+        answer: "parentheses",
+    },
+    {
+        title: "Arrays in JavaScript can be used to store ____.",
+        choices: ["numbers and strings", "other arrays", "booleans", "all of the above"],
+        answer: "all of the above",
+    },
+    {
+        title: "String values must be enclosed within _____ when being assigned to variables.",
+        choices: ["commas", "curly brackets", "quotes", "parenthesis"],
+        answer: "quotes",
+    },
+    {
+        title: "A very useful tool used during development and debugging for printing content to the debugger is",
+        choices: ["JavaScript", "terminal/bash", "for loops", "console.log"],
+        answer: "console.log",
+    },
+];
 var quizPage = 0;
-var score = 100;
 
-
-function quizPopulate (quizPage) {
+function quizPopulate(quizPage) {
     content.innerHTML = "";
-
+        
     content.appendChild(newH2);
 
-    newH2.textContent = quizQue[quizPage];
+    newH2.textContent = quizBody[quizPage].title;
 
     for (var i = 0; i < 4; i++) {
         quizButton = document.createElement("button");
 
-        if (i === 0) {
-            quizButton.classList.add("correct");
-        } else {
-            quizButton.classList.add("wrong");
-        }
-
         content.appendChild(quizButton);
 
-        quizButton.textContent = quizAns[quizPage * 4 + i];
+        quizButton.textContent = quizBody[quizPage].choices[i];
 
         quizButton.addEventListener("click", function (event) {
             event.preventDefault();
-
-            if (event.target.matches(".wrong")) {
+            var selectedAnswer = event.target.textContent;
+            if (selectedAnswer !== quizBody[quizPage].answer) {
                 timeRemaining = timeRemaining - 5;
-                score = score - 20;
+                mark.appendChild(newHr);
+                mark.appendChild(newP);
+                newP.textContent = "Wrong!";
+            } else {
+                mark.appendChild(newHr);
+                mark.appendChild(newP);
+                newP.textContent = "Correct!";
             }
-            
-            quizPopulate(quizPage +1);
+
+            quizPage++;
+
+            if (quizPage < quizBody.length) {
+                quizPopulate(quizPage);
+            } else {
+                clearInterval(timerInterval);
+                highScoreScreen();
+            }
         })
     };
 }
